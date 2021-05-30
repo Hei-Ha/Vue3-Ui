5c79485e72e36ef25f0931876a5c1302af71deff
wangchuang <wangchuang.free@gmail.com>
2021-05-30 16:57:09 +0800

    feat: 完善aside 逻辑

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="topnav">
-      <div class="logo" @click="toggleAside">LOGO</div>
+      <div class="logo">LOGO</div>
       <ul class="menu">
         <li>菜单一</li>
         <li>菜单二</li>
       </ul>
-    </div>
+    <span class="toggleAside" @click="toggleAside" />
+  </div>
 </template>
 <script>
 import { defineComponent, inject } from 'vue'
@@ -31,6 +32,8 @@ export default defineComponent({
   padding: 16px;
   position: relative;
   z-index: 1;
+  justify-content: center;
+  align-items: center;
   > .logo {
     max-width: 6em;
     margin-right: auto;
@@ -42,6 +45,21 @@ export default defineComponent({
     > li {
       margin: 0 1em;
     }
+  }
+  > .toggleAside {
+    display: none;
+    width: 24px;
+    height: 24px;
+    background: red;
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  @media (max-width:500px) {
+    > .menu{display: none;}
+    > .logo{margin: 0 auto;}
+    > .toggleAside {display: inline-block;}
   }
 }
 </style>
