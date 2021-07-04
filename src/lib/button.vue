a47ac66153b29b31b109b681586e370aed7faeca
wangchuang <wangchuang.free@gmail.com>
2021-07-04 21:40:20 +0800

    完善button基础样式

--- src/lib/button.vue
+++ src/lib/button.vue
@@ -2,9 +2,7 @@
   <!--  vue 会默认把外面穿进来的方法传到这里的最外面的标签上（这里是  div）-->
   <!--  可以使用 $attrs 获取事件再重新赋值给想要的元素 -->
   <div>
-    <button
-        v-bind="$attrs"
-        :class="`theme-${theme}`">
+    <button v-bind="$attrs" class="WButton" :class="`theme-${theme}`">
       <slot />
     </button>
   </div>
@@ -23,6 +21,43 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+$h: 32px;
+$border-color: #d9d9d9;
+$color: #333;
+$blue: #40a9ff;
+$radius: 4px;
+.WButton {
+  box-sizing: border-box;
+  height: $h;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background: white;
+  color: $color;
+  border: 1px solid $border-color;
+  border-radius: $radius;
+  box-shadow: 0 1px 0 fade-out(black, 0.95);
 
+  & + & {
+    margin-left: 8px;
+  }
+
+  &:hover,
+  &:focus {
+    color: $blue;
+    border-color: $blue;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &::-moz-focus-inner {
+    border: 0;
+  }
+}
 </style>
