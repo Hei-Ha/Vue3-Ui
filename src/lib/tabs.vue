b0c48ca4a757e0e7bfc6f73ae71ce01f895d7c9a
wangchuang <wangchuang.free@gmail.com>
2021-07-12 22:24:58 +0800

    完成 tab

--- src/lib/tabs.vue
+++ src/lib/tabs.vue
@@ -1,17 +1,30 @@
 <template>
   <div class="W-tabs">
-    <div class="W-tabs-nav">
-      <div class="W-tabs-nav-item" :class="{selected: t === selected}" v-for="(t, index) in titles" :key="t + index" @click="select(t)">{{ t }}</div>
+    <div class="W-tabs-nav" ref="container">
+      <div
+        class="W-tabs-nav-item"
+        :class="{selected: t === selected}"
+        v-for="(t, index) in titles"
+        :key="t + index"
+        :ref="el => {if (t === selected) selectedItem = el}"
+        @click="select(t)"
+      >
+        {{ t }}
+      </div>
+      <div
+        class="W-tabs-nav-indicator"
+        ref="indicator"
+      />
     </div>
     <div class="W-tabs-content">
-<!--      <component class="W-tabs-content-item" :is="current" />-->
-      <component class="W-tabs-content-item" v-for="(c, index) in defaults" :key="index + c" :is="c" :class="{selected: c.props.title === selected}" />
+      <component :is="current" :key="current.props.title" />
+<!--      <component class="W-tabs-content-item" v-for="(c, index) in defaults" :key="index + c" :is="c" :class="{selected: c.props.title === selected}" />-->
     </div>
   </div>
 </template>
 
 <script lang="ts">
-// import { computed } from 'vue'
+import { ref, onMounted, onUpdated, watchEffect, computed } from 'vue'
 import Tab from './tab.vue'
 
 export default {
@@ -22,30 +35,57 @@ export default {
     }
   },
   setup(props, context) {
+    const selectedItem = ref<HTMLDivElement | null>(null)
+    const indicator = ref<HTMLDivElement | null>(null)
+    const container = ref<HTMLDivElement | null>(null)
+
     const defaults = context.slots.default()
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
         throw new Error('子标签必须是 tab')
       }
     })
+    const current = computed(() => {
+      return defaults.find((tag) => tag.props.title === props.selected)
+    })
+
+    onMounted(() => {
+      const { width } = selectedItem.value.getBoundingClientRect()
+      indicator.value.style.width = width + 'px'
+      const { left:containerLeft } = container.value.getBoundingClientRect()
+      const { left } = selectedItem.value.getBoundingClientRect()
+      indicator.value.style.left = (left - containerLeft) + 'px'
+      // watchEffect(() => {
+      //   const { width } = selectedItem.value.getBoundingClientRect()
+      //   indicator.value.style.width = width + 'px'
+      //   const { left:containerLeft } = container.value.getBoundingClientRect()
+      //   const { left } = selectedItem.value.getBoundingClientRect()
+      //   indicator.value.style.left = (left - containerLeft) + 'px'
+      // })
+    })
+    onUpdated(() => {
+      const { width } = selectedItem.value.getBoundingClientRect()
+      indicator.value.style.width = width + 'px'
+      const { left:containerLeft } = container.value.getBoundingClientRect()
+      const { left } = selectedItem.value.getBoundingClientRect()
+      indicator.value.style.left = (left - containerLeft) + 'px'
+    })
+
     const titles = defaults.map((item) => {
       return item.props.title
     })
-    // 渲染内容
-    // const current = computed(() => {
-    //   return defaults.filter((tag) => {
-    //     return tag.props.title === props.selected
-    //   })[0]
-    // })
     // title 点击事件函数
     const select = (t: string) => {
       context.emit('update:selected', t)
     }
     return {
+      selectedItem,
       defaults,
       titles,
-      // current,
-      select
+      current,
+      select,
+      indicator,
+      container
     }
   }
 }
@@ -60,6 +100,7 @@ $border-color: #d9d9d9;
      display: flex;
      color: $color;
      border-bottom: 1px solid $border-color;
+    position: relative;
     &-item {
        padding: 8px 0;
        margin: 0 16px;
@@ -71,17 +112,25 @@ $border-color: #d9d9d9;
          color: $blue;
       }
     }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms;
+    }
   }
   &-content {
     padding: 8px 0;
-
-    &-item {
-      display: none;
-
-      &.selected {
-        display: block;
-      }
-    }
+    //&-item {
+    //  display: none;
+    //
+    //  &.selected {
+    //    display: block;
+    //  }
+    //}
    }
 }
 </style>
