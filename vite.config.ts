2aeed44fe2ea45ab7bbcfcfac2529f45a9885c9f
wangchuang <wangchuang.space@gmail.com>
2022-02-06 00:23:59 +0800

    完善

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,34 @@ import vue from '@vitejs/plugin-vue'
 import path from "path"
 import fs from 'fs'
 import { baseParse } from '@vue/compiler-core'
-import { md } from './plugins/md'
+import { marked } from 'marked'
+
+const mdToJs = str => {
+    const content = JSON.stringify(marked(str))
+    return `export default ${content}`
+}
+function md() {
+    return {
+        // 用于开发
+        configureServer: [
+            async ({ app }) => {
+                app.use(async (ctx, next) => { // koa
+                    if (ctx.path.endsWith('.md')) {
+                        ctx.type = 'js'
+                        const filePath = path.join(process.cwd(), ctx.path)
+                        ctx.body = mdToJs(fs.readFileSync(filePath).toString())
+                    } else {
+                        await next()
+                    }
+                })
+            },
+        ],
+        transforms: [{  // 用于 rollup // 插件
+            test: context => context.path.endsWith('.md'),
+            transform: ({ code }) => mdToJs(code)
+        }]
+    }
+}
 
 export default {
     assetsDir: 'assets',
